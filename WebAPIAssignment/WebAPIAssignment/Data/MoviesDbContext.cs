0dd7d69fcd76eab5bb0ce584fe3a140755cc0521
charan1805 <sreecharanmudhiraj@gmail.com>
2023-09-12 22:26:57 +0530

    first class

---
+++ WebAPIAssignment/WebAPIAssignment/Data/MoviesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAssignment.Models;
+
+namespace WebAPIAssignment.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext (DbContextOptions<MoviesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIAssignment.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ WebAPIAssignment/WebAPIAssignment/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIAssignment.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Director { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+    }
+}
